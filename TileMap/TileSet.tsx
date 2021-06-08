8d8be1215cc46390e110ceac0a8faae9c8a31950
Anna Caroline <58178338+annaosousa@users.noreply.github.com>
2021-06-08 17:56:45 -0300

    Add files via upload

---
+++ TileMap/TileSet.tsx
@@ -0,0 +1,79 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.2" tiledversion="1.3.4" name="tileset" tilewidth="64" tileheight="64" tilecount="25" columns="0">
+ <grid orientation="orthogonal" width="1" height="1"/>
+ <tile id="0">
+  <image width="32" height="32" source="../ConsoleApplication1/armadilha_direita.png"/>
+ </tile>
+ <tile id="1">
+  <image width="32" height="32" source="../ConsoleApplication1/armadilha_esquerda.png"/>
+ </tile>
+ <tile id="2">
+  <image width="32" height="32" source="../ConsoleApplication1/espinho.png"/>
+ </tile>
+ <tile id="3">
+  <image width="32" height="32" source="../ConsoleApplication1/espinho_direita.png"/>
+ </tile>
+ <tile id="4">
+  <image width="32" height="32" source="../ConsoleApplication1/espinho_esquerda.png"/>
+ </tile>
+ <tile id="5">
+  <image width="32" height="32" source="../ConsoleApplication1/fundo.png"/>
+ </tile>
+ <tile id="6">
+  <image width="32" height="32" source="../ConsoleApplication1/parede.png"/>
+ </tile>
+ <tile id="7">
+  <image width="32" height="32" source="../ConsoleApplication1/parede_up.png"/>
+ </tile>
+ <tile id="8">
+  <image width="32" height="32" source="../ConsoleApplication1/porta.png"/>
+ </tile>
+ <tile id="9">
+  <image width="32" height="32" source="../ConsoleApplication1/vazio.png"/>
+ </tile>
+ <tile id="10">
+  <image width="64" height="64" source="../ConsoleApplication1/teste.png"/>
+ </tile>
+ <tile id="11">
+  <image width="32" height="32" source="../../../../OneDrive/Imagens/fogo.png"/>
+ </tile>
+ <tile id="12">
+  <image width="32" height="32" source="../../../../OneDrive/Imagens/fogo.png"/>
+ </tile>
+ <tile id="13">
+  <image width="32" height="32" source="../ConsoleApplication1/parede_clara.png"/>
+ </tile>
+ <tile id="14">
+  <image width="32" height="32" source="../ConsoleApplication1/fundo1.png"/>
+ </tile>
+ <tile id="15">
+  <image width="32" height="32" source="../ConsoleApplication1/prisao.png"/>
+ </tile>
+ <tile id="16">
+  <image width="32" height="32" source="../ConsoleApplication1/teste_fundo.png"/>
+ </tile>
+ <tile id="17">
+  <image width="32" height="32" source="../ConsoleApplication1/espinho_fundo.png"/>
+ </tile>
+ <tile id="18">
+  <image width="32" height="32" source="../ConsoleApplication1/teste_chama.png"/>
+ </tile>
+ <tile id="19">
+  <image width="32" height="32" source="../ConsoleApplication1/teste_chama.png"/>
+ </tile>
+ <tile id="20">
+  <image width="32" height="32" source="../ConsoleApplication1/teste_chama.png"/>
+ </tile>
+ <tile id="21">
+  <image width="32" height="32" source="../ConsoleApplication1/teste_fundo2.png"/>
+ </tile>
+ <tile id="22">
+  <image width="32" height="32" source="../ConsoleApplication1/espinho_fundo2.png"/>
+ </tile>
+ <tile id="23">
+  <image width="32" height="32" source="../ConsoleApplication1/esquerda2.png"/>
+ </tile>
+ <tile id="24">
+  <image width="32" height="32" source="../ConsoleApplication1/direita2.png"/>
+ </tile>
+</tileset>
