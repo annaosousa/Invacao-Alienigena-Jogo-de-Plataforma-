f1353185516c83de7fb6cec9dfc9ccd492610ae6
Anna Caroline <58178338+annaosousa@users.noreply.github.com>
2021-06-11 11:45:22 -0300

    Delete Jogo directory

--- Jogo/ConsoleApplication1.h
+++
@@ -1,3 +0,0 @@
-int main();
-
-void NewFunction(Player& player, float deltaTime);
--- Jogo/Cor.cpp
+++
@@ -1,5 +0,0 @@
-#include "Cor.h"
-
-namespace InvasaoAlienigena {
-    Cor::Cor(unsigned char R, unsigned char G, unsigned char B, unsigned char A) : r{ R }, g{ G }, b{ B }, a{ A } {}
-}
